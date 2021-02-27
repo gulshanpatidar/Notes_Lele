005765adeaa6607c8852cc592f0757d643c5515b
gulshanpatidar <jankarikibaat@gmail.com>
2021-02-27 11:37:28 +0530

    another test 1.

--- app/src/main/res/layout/Note.kt
+++ app/src/main/res/layout/Note.kt
@@ -10,4 +10,5 @@ import androidx.room.PrimaryKey
 class Note(@ColumnInfo(name = "text")val text: String) {
     //annotated that this will be also a column and it is primary key and turned the auto generate on so that we don't have to pass the id each time. and that's why it is not passed into the constructor instead it is defined inside the class itself.
     @PrimaryKey(autoGenerate = true)val id = 0
+    //kindly ignore this comment.
 }

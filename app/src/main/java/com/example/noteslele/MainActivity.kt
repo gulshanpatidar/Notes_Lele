6e0265b3161590e0a1cb9f2a2629afadd0c865c7
gulshanpatidar <jankarikibaat@gmail.com>
2021-02-27 20:32:02 +0530

    implemented the viewModel successfully.

--- app/src/main/java/com/example/noteslele/MainActivity.kt
+++ app/src/main/java/com/example/noteslele/MainActivity.kt
@@ -3,16 +3,26 @@ package com.example.noteslele
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.noteslele.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     //create the variable for data binding
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: NoteViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //set the data binding
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        //create an instance of the viewModel
+        viewModel = ViewModelProvider(this,ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(NoteViewModel::class.java)
+        //add the lifecycle observer for the all Notes such that when the data changes, it can perform the appropriate actions
+        viewModel.allNotes.observe(this, Observer {
+
+        })
     }
 }
--- app/src/main/java/com/example/noteslele/NoteViewModel.kt
+++ app/src/main/java/com/example/noteslele/NoteViewModel.kt
@@ -1,4 +1,20 @@
 package com.example.noteslele
 
-class NoteViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+
+    //variable for all the notes
+    val allNotes: LiveData<List<Note>>
+
+    init {
+        //create an instance of the dao so that we can pass it to constructor of the NoteDatabase and create and instance of the NoteRepository
+        val dao = NoteDatabase.getDatabase(application).getNoteDao()
+        //create instance of the repository with the help of dao
+        val repository = NoteRepository(dao)
+        //get the notes from the repository
+        allNotes = repository.allNotes
+    }
 }
